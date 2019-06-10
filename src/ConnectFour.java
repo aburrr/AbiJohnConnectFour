204706cd4a07f52a06b0a52c155a9fc59412811f
john_stafford <John_stafford@student.wayland.k12.ma.us>
2019-06-10 12:18:25 -0400

    asdfasdfasdf

--- src/ConnectFour.java
+++ src/ConnectFour.java
@@ -4,33 +4,31 @@ import java.awt.event.*;
 
 
 public class ConnectFour extends JPanel{
+    private int[][] board = new int[6][7];
+    private Spaces[][] spaces = new Spaces[6][7];
+    private boolean color;
 
-    int[][] board = new int[6][7];
-    private int spotY, spotX;
 
 
     public ConnectFour(int w, int h) {
-        for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board[r].length; c++) {
-                board[r][c] = 0;
-            }
-        }
-    spotY = 0;
-    spotX = 0;
-    setUpMouseListener();
+        setUpMouseListener();
 
     }
+
     public void setUpMouseListener(){
+
         addMouseListener(new MouseListener() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 int x = e.getX();
                 int y = e.getY();
-
                     for (int r = board.length - 1; r > 0; r--) {
                         if(x < 100 && y < 200 && y > 100){
-                            if(board[r][0] == 0){
+                            if(board[r][0] == 0 && color == false){
                                 board[r][0] = 1;
+                                break;
+                            }else if(board[r][0] == 0 && color == true){
+                                board[r][0] = 2;
                                 break;
                             }
 
@@ -38,53 +36,71 @@ public class ConnectFour extends JPanel{
                     }
                 for (int r = board.length -1; r > 0; r--) {
                     if(x < 200 && x > 100 && y < 200 && y > 100){
-                        if(board[r][1] == 0){
+                        if(board[r][1] == 0 && color == false){
                             board[r][1] = 1;
+                            break;
+                        }else if(board[r][1] == 0 && color == true){
+                            board[r][1] = 2;
                             break;
                         }
                     }
                 }
                 for (int r = board.length - 1; r > 0; r--) {
                     if(x < 300 && x > 200 && y < 200 && y > 100){
-                        if(board[r][2] == 0){
+                        if(board[r][2] == 0 && color == false){
                             board[r][2] = 1;
+                            break;
+                        }else if (board[r][2] == 0 && color == true){
+                            board[r][2] = 2;
                             break;
                         }
                     }
                 }
                 for (int r = board.length -1; r > 0; r--) {
                     if(x < 400 && x > 300 && y < 200 && y > 100){
-                        if(board[r][3] == 0){
+                        if(board[r][3] == 0 && color == false){
                             board[r][3] = 1;
+                            break;
+                        }else if(board[r][3] == 0 && color == true){
+                            board[r][3] = 2;
                             break;
                         }
                     }
                 }
                 for (int r = board.length - 1; r > 0; r++) {
                     if(x < 500 && x > 400 && y < 200 && y > 100){
-                        if(board[r][4] == 0){
+                        if(board[r][4] == 0 && color == false){
                             board[r][4] = 1;
+                            break;
+                        }else if(board[r][4] == 0 && color == true){
+                            board[r][4] = 2;
                             break;
                         }
                     }
                 }
                 for (int r = board.length - 1; r > 0; r--) {
                     if(x < 600 && x > 500 && y < 200 && y > 100){
-                        if(board[r][5] == 0){
+                        if(board[r][5] == 0 && color == false){
                             board[r][5] = 1;
+                            break;
+                        }else if(board[r][5] == 0 && color == true){
+                            board[r][5] = 2;
                             break;
                         }
                     }
                 }
                 for (int r = board.length - 1; r > 0; r--) {
                     if(x < 700 && x > 600 && y < 200 && y > 100){
-                        if(board[r][6] == 0){
+                        if(board[r][6] == 0 && color == false){
                             board[r][6] = 1;
+                            break;
+                        }else if(board[r][6] == 0 && color == true){
+                            board[r][6] = 2;
                             break;
                         }
                     }
                 }
-
+            color = !color;
             }
 
             @Override
@@ -111,27 +127,24 @@ public class ConnectFour extends JPanel{
 
     }
 
-    public void draw(Graphics2D g2) {
-        boolean color = true;
-        if (color == true) {
-            g2.setColor(Color.RED);
-        } else {
-            g2.setColor(Color.YELLOW);
-        }
-
-        for (int r = 0; board.length < 0; r++) {
-            for (int c = 0; board[0].length < 0; c++) {
-                if (board[r][c] == 1) {
-                    g2.fillOval(100 * r + 200, 100 * c, 100, 100);
-                    color = !color;
-                }
-            }
-        }
-    }
 
 
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[r].length; c++) {
+                if(board[r][c] == 1){
+                    g2.setColor(Color.red);
+                    g2.fillOval(100 * c , 100 * r + 200, 100, 100);
+                }else if(board[r][c] == 2){
+                    g2.setColor(Color.yellow);
+                    g2.fillOval(100 * c , 100 * r + 200, 100, 100);
+                }
+
+            }
+        }
+
+
         g2.setColor(Color.BLACK);
         g2.drawLine(100,200,100,800);
         g2.drawLine(200,200,200,800);
@@ -145,5 +158,6 @@ public class ConnectFour extends JPanel{
         g2.drawLine(0,500,700,500);
         g2.drawLine(0,600,700,600);
         g2.drawLine(0,700,700,700);
+        repaint();
     }
 }
